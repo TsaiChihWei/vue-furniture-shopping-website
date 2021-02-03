41808dd9b3ced05382bd4b29e14b03ff3fa2364d
Robin Tsai <a0963252517@gmail.com>
2021-02-03 22:05:49 +0800

    finished footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mt-4 d-flex justify-content-center">
+        <div class="container row">
+            <div class="col-12 col-md-6">
+                <img src="../assets/images/logo.png" alt="logo">
+                <p class="footer-info mt-2">歡迎預約參訪 ! 此平台提供優質的服務及居家品項
+                    <br>
+                    “用一杯品茶的時光，乘坐體驗在那有手作溫度的座椅上，細細觀看那如詩畫般的夕陽午後，人生不過如此“
+                </p>
+            </div>
+            <div class="col-12 col-md-6 mt-3 d-flex flex-column justify-content-center">
+                <div class="subtitle subscribe-btn mb-2">訂閱電子報</div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="請輸入信箱" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-outline-secondary" type="button" id="button-addon2">訂閱</button>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .footer-info {
+        color: #aaaaaa;
+    }
+    .subscribe-btn {
+        font-size: 24px;
+        font-weight: bold;
+    }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <Nav></Nav>
     <Main></Main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
   import Nav from './Nav'
   import Main from './Main'
+  import Footer from './Footer'
 
   export default {
     components: {
       Nav,
       Main,
+      Footer,
     },
     name: 'Home',
     data() {
@@ -25,57 +28,5 @@
 
 
 <style lang="scss">
-  .title {
-    font-size: 24px;
-    font-weight: bold;
-  }
-
-  .subtitle {
-    color: #aaaaaa;
-  }
-
-  .description {
-    color: #777777;
-  }
-
-  .feature+.feature {
-    margin-top: 20px;
-  }
-
-  /* 關注我們 social-links */
-  .social-links .subtitle {
-    margin-bottom: 20px;
-    font-size: 36px;
-    color: #7ab3b3;
-  }
-
-  .links a {
-    color: #7ab3b3;
-  }
-
-  .links a i {
-    margin-right: 10px;
-    font-size: 28px;
-    color: #7ab3b3;
-  }
-
-  /* 熱門產品分類 */
-  .category {
-    position: relative;
-    transition: .3s all;
-  }
-  .category:hover {
-    transform: translateY(-20px);
-  }
-  .category .subtitle {
-    position: absolute;
-    top: 40px;
-    font-size: 24px;
-    color: #000;
-  }
-  /* @media screen and (min-width: 992px) {
-      .banner {
-        max-width: 1200px;
-      }
-    } */
+  
 </style>
