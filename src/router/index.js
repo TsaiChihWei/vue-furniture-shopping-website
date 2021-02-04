afea9d4fae90e93673c28e1fa2bf1d15bef005f9
Robin Tsai <a0963252517@gmail.com>
2021-02-04 14:34:22 +0800

    add manager Dashboard.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/components/Home'
+import Dashboard  from '@/components/manager/Dashboard'
+import ManageProducts  from '@/components/manager/ManageProducts'
 
 Vue.use(Router)
 
@@ -10,6 +13,18 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
-    }
+    },
+    {
+      path: '/admin',
+      // name: 'Dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: '',
+          name: 'ManageProducts',
+          component: ManageProducts,
+        },
+      ]
+    },
   ]
 })
